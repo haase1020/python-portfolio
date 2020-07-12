54646faedb80701aca4b4b5d72e06c3a8ee49a54
Jon Haase <haase1020@gmail.com>
2020-07-12 06:07:24 -0400

    learning flask

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    # print(url_for('static', filename='favicon.ico'))
+    return render_template('index.html', name=username, post_id=post_id)
 
 
 @app.route('/blog')
